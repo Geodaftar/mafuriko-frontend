91bdff3fbcb0189a20b22aa36869bd61bb774c4e
Salomon Diei <salomondiei08@gmail.com>
2024-07-05 05:11:06 +0000

    style: format main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mafuriko/features/maruriko_app.dart';
 import 'service_locator.dart' as sl;
-void main() async{
-    await sl.init();
+
+void main() async {
+  await sl.init();
   runApp(const MafurikoApp());
 }
-
