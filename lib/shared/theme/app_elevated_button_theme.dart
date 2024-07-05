bcf8241953518278e6007b8874aa46709938e289
Salomon Diei <salomondiei08@gmail.com>
2024-07-05 04:55:24 +0000

    feat: add text theme

--- lib/shared/theme/app_elevated_button_theme.dart
+++ lib/shared/theme/app_elevated_button_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mafuriko/shared/theme/app_color_scheme.dart';
+import 'package:mafuriko/shared/theme/app_text_theme.dart';
 
 final appElevatedButtonThemeLight = ElevatedButtonThemeData(
   style: ElevatedButton.styleFrom(
