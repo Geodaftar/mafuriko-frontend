f274cabe37c00a566a5ab9b7d1989eb7d149ab93
Salomon Diei <salomondiei08@gmail.com>
2024-07-05 03:43:01 +0000

    feat: add endpoint class

---
+++ lib/shared/helpers/endpoints.dart
@@ -0,0 +1,14 @@
+class Endpoints {
+  Endpoints._();
+
+  // base url
+  static const String baseUrl = "http://localhost:3000/v1.0";
+  static const String placesUrl = 'https://places.googleapis.com/v1/places';
+
+  // receiveTimeout
+  static const int receiveTimeout = 15000;
+
+  // connectTimeout
+  static const int connectionTimeout = 15000;
+
+}
