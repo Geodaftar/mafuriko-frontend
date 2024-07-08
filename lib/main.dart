25e1a78cfcfeb0f55478b1b957d1744457d61d3b
Cfcool100 <cfcool100@gmail.com>
2024-07-08 09:44:58 +0100

    [cf_onboarding] : 🚚 rename file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mafuriko/features/maruriko_app.dart';
+import 'package:mafuriko/features/mafuriko_app.dart';
 import 'service_locator.dart' as sl;
 
 void main() async {
