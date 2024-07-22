1c6b9e4c9e32f2df140966c991d45a1587905fa3
Cfcool100 <cfcool100@gmail.com>
2024-07-22 21:58:10 +0100

    [cf_auth] : login use case

---
+++ lib/features/authentication/domain/usecases/login_usecase.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:mafuriko/features/authentication/domain/entities/user_entity.dart';
+import 'package:mafuriko/features/authentication/domain/repositories/auth_repository.dart';
+import 'package:mafuriko/shared/base/usecase.dart';
+import 'package:mafuriko/shared/errors/failures.dart';
+
+class LoginUseCase extends UseCase<UserEntity, LogInParams> {
+  final AuthRepository repository;
+
+  LoginUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, UserEntity>> call(LogInParams params) async {
+    return await repository.login(
+      userEmail: params.email,
+      userPassword: params.password,
+    );
+  }
+}
+
+class LogInParams {
+  final String email;
+  final String password;
+
+  LogInParams({
+    required this.email,
+    required this.password,
+  });
+}
