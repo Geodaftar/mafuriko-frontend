3faaabba647594be4c8ecdcf20b2cf635a92ffe4
Cfcool100 <cfcool100@gmail.com>
2024-07-22 21:50:27 +0100

    [cf_auth] : 🔥 remove the generated data class

--- lib/features/authentication/domain/entities/user_entity.dart
+++ lib/features/authentication/domain/entities/user_entity.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 class UserEntity {
   final String? id;
   final String? userEmail;
@@ -19,54 +17,4 @@ class UserEntity {
     this.confirmPassword,
     this.image,
   });
-
-  UserEntity copyWith({
-    String? id,
-    String? userEmail,
-    String? userName,
-    String? userNumber,
-    String? userPassword,
-    String? confirmPassword,
-    String? image,
-  }) {
-    return UserEntity(
-      id: id ?? this.id,
-      userEmail: userEmail ?? this.userEmail,
-      userName: userName ?? this.userName,
-      userNumber: userNumber ?? this.userNumber,
-      userPassword: userPassword ?? this.userPassword,
-      confirmPassword: confirmPassword ?? this.confirmPassword,
-      image: image ?? this.image,
-    );
-  }
-
-  Map<String, dynamic> toMap() => {
-        '_id': id,
-        'userEmail': userEmail,
-        'userName': userName,
-        'userNumber': userNumber,
-        'userPassword': userPassword,
-        'image': image,
-      };
-
-  factory UserEntity.fromMap(Map<String, dynamic> map) {
-    return UserEntity(
-      id: map['id'],
-      userEmail: map['userEmail'],
-      userName: map['userName'],
-      userNumber: map['userNumber'],
-      userPassword: map['userPassword'],
-      image: map['image'],
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory UserEntity.fromJson(String source) =>
-      UserEntity.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'UserEntity(id: $id, userEmail: $userEmail, userName: $userName,  userNumber: $userNumber, userPassword: $userPassword, image: $image)';
-  }
 }
