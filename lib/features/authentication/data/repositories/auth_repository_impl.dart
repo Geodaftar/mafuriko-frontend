eefef0fde2023f9d135735696ca113cac2379837
Cfcool100 <cfcool100@gmail.com>
2024-07-22 21:55:06 +0100

    [cf_auth] : :sparkles: update auth repo ft

--- lib/features/authentication/data/repositories/auth_repository_impl.dart
+++ lib/features/authentication/data/repositories/auth_repository_impl.dart
@@ -1,6 +1,9 @@
 import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
+
+import 'package:mafuriko/shared/helpers/network_info.dart';
+import 'package:mafuriko/features/authentication/data/datasources/local/auth_local_data_source.dart';
 import 'package:mafuriko/features/authentication/data/datasources/remote/auth_remote_data_source.dart';
 import 'package:mafuriko/features/authentication/domain/entities/user_entity.dart';
 import 'package:mafuriko/features/authentication/domain/repositories/auth_repository.dart';
@@ -9,8 +12,14 @@ import 'package:mafuriko/shared/errors/failures.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource dataSource;
+  final AuthLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
 
-  AuthRepositoryImpl({required this.dataSource});
+  AuthRepositoryImpl({
+    required this.dataSource,
+    required this.localDataSource,
+    required this.networkInfo,
+  });
 
   @override
   Future<Either<Failure, UserEntity>> signUp({
@@ -28,10 +37,51 @@ class AuthRepositoryImpl implements AuthRepository {
         userPassword: userPassword,
         confirmPassword: confirmPassword,
       );
-      return Right(data!);
+
+      localDataSource.cacheUser(data);
+      return Right(data);
     } on ServerException catch (e) {
       log(e.message);
       return Left(ServerFailure(message: e.message));
     }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> login(
+      {required String userEmail, required String userPassword}) async {
+    try {
+      final data = await dataSource.login(
+          userEmail: userEmail, userPassword: userPassword);
+
+      log('from auth repo impl :: login request ::: data **** ${data.toJson()}');
+
+      localDataSource.cacheUser(data);
+
+      return Right(data);
+    } on ServerException catch (e) {
+      log('::::::::::${e.message}');
+      return Left(ServerFailure(message: e.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> isLoggedIn() async {
+    try {
+      final localUser = await localDataSource.getCachedUser();
+      if (localUser != null) {
+        log('message from auth repo impl ::::: localUser ==== ${localUser.toJson()}');
+        return Right(localUser);
+      } else {
+        return const Left(CacheFailure(
+            message: 'Une Erreur est survenue lors de la mise en cache.'));
+      }
+    } on CacheException catch (e) {
+      return Left(CacheFailure(message: e.message));
+    }
+  }
+
+  @override
+  Future<void> logout() async {
+    await localDataSource.clearCachedUser();
   }
 }
