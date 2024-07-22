beebe9adc360a8c740cbba6480dde1619cd80fd1
Cfcool100 <cfcool100@gmail.com>
2024-07-22 21:41:02 +0100

    [cf_auth] : general app button

---
+++ lib/shared/widgets/buttons.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mafuriko/gen/fonts.gen.dart';
+
+class PrimaryExpandedButton extends StatelessWidget {
+  const PrimaryExpandedButton({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+  final VoidCallback onTap;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            onPressed: onTap,
+            child: Text(
+              title,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.sp,
+                fontFamily: AppFonts.inter,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
