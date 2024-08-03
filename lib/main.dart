401d89f044f260cec0476289afb6bcba731fe3ef
Cfcool100 <cfcool100@gmail.com>
2024-08-03 17:05:46 +0100

    [cf_f_pass] : :wrench: fb setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mafuriko/features/mafuriko_app.dart';
+import 'firebase_options.dart';
 import 'service_locator.dart' as sl;
 import 'shared/helpers/bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await sl.init();
   Bloc.observer = const AppBlocObserver();
