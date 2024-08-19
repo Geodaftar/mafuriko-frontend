4a3fff52136ddeb1c5fca2bd3ca25e37c80db2c0
Cfcool100 <cfcool100@gmail.com>
2024-08-19 20:58:57 +0100

    [cf_f_pass] : 🔗new routes + use shellRoute for persisting bottom navBar

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mafuriko/core/routes/constant_path.dart';
+import 'package:mafuriko/features/app_scaffold.dart';
 // import 'package:mafuriko/features/authentication/presentation/blocs/bloc/auth_bloc.dart';
 import 'package:mafuriko/features/authentication/presentation/screens/forgot_password.dart';
 import 'package:mafuriko/features/authentication/presentation/screens/verify_number.dart';
 import 'package:mafuriko/features/authentication/presentation/screens/login_screen.dart';
 import 'package:mafuriko/features/authentication/presentation/screens/otp_screen.dart';
 import 'package:mafuriko/features/authentication/presentation/screens/sign_up_screen.dart';
+import 'package:mafuriko/features/home/presentation/screens/alert_detail_screen.dart';
+import 'package:mafuriko/features/home/presentation/screens/alerts_list_screen.dart';
 import 'package:mafuriko/features/home/presentation/screens/home_screen.dart';
 import 'package:mafuriko/features/onboarding/screens/onboarding_view.dart';
 
@@ -18,67 +21,113 @@ final GoRouter router = GoRouter(
       builder: (BuildContext context, GoRouterState state) {
         return const OnboardingView();
       },
-      // redirect: (context, state) {
-      //   final authBloc = context.read<AuthBloc>();
-      //   final bool isAuthenticated = authBloc.state is AuthSuccess;
+      routes: [
+        GoRoute(
+          path: Paths.signUp,
+          name: Paths.signUp,
+          builder: (context, state) {
+            return const SignUpScreen();
+          },
+        ),
+        GoRoute(
+          path: Paths.login,
+          name: Paths.login,
+          builder: (context, state) {
+            return const LoginScreen();
+          },
+        ),
+        ShellRoute(
+            builder: (context, state, child) {
+              return ScaffoldApp(child: child);
+            },
+            routes: [
+              GoRoute(
+                  path: Paths.home,
+                  name: Paths.home,
+                  builder: (context, state) {
+                    return const HomeScreen();
+                  },
+                  routes: [
+                    GoRoute(
+                      path: Paths.alertsScreen,
+                      name: Paths.alertsScreen,
+                      pageBuilder: (context, state) {
+                        return CustomTransitionPage(
+                          key: state.pageKey,
+                          child: const AlertsListScreen(),
+                          transitionsBuilder:
+                              (context, animation, secondaryAnimation, child) {
+                            const begin = Offset(1.0, 0.0);
+                            const end = Offset.zero;
+                            const curve = Curves.easeInOut;
 
-      //   debugPrint('Current path: ${state.path}');
-      //   debugPrint('Is authenticated: $isAuthenticated');
-      //   debugPrint('AuthBloc state: ${authBloc.state.runtimeType}');
+                            var tween = Tween(begin: begin, end: end)
+                                .chain(CurveTween(curve: curve));
+                            var offsetAnimation = animation.drive(tween);
 
-      //   if (!isAuthenticated && state.path != Paths.initialPath) {
-      //     return Paths.initialPath;
-      //   } else if (isAuthenticated && state.path == Paths.initialPath) {
-      //     return '/${Paths.home}';
-      //   }
-      //   return null;
-      // },
-    ),
-    GoRoute(
-      path: '/${Paths.signUp}',
-      name: Paths.signUp,
-      builder: (context, state) {
-        return const SignUpScreen();
-      },
-    ),
-    GoRoute(
-      path: '/${Paths.login}',
-      name: Paths.login,
-      builder: (context, state) {
-        return const LoginScreen();
-      },
-    ),
-    GoRoute(
-      path: '/${Paths.home}',
-      name: Paths.home,
-      builder: (context, state) {
-        return const HomeScreen();
-      },
-    ),
-    GoRoute(
-      path: '/${Paths.verifyNumber}',
-      name: Paths.verifyNumber,
-      builder: (context, state) {
-        return const VerifyNumberScreen();
-      },
-    ),
-    GoRoute(
-      path: '/${Paths.forgotPassword}',
-      name: Paths.forgotPassword,
-      builder: (context, state) {
-        return const ForgotPasswordScreen();
-      },
-    ),
-    GoRoute(
-      path: '/${Paths.otpScreen}',
-      name: Paths.otpScreen,
-      builder: (context, state) {
-        final List data = state.extra as List;
-        return OTPScreen(
-          vId: data[1],
-          phoneNumber: data[0],
-        );
-      },
+                            return SlideTransition(
+                              position: offsetAnimation,
+                              child: child,
+                            );
+                          },
+                        );
+                      },
+                      routes: [
+                        GoRoute(
+                          path: Paths.alertDetailScreen,
+                          name: Paths.alertDetailScreen,
+                          pageBuilder: (context, state) {
+                            return CustomTransitionPage(
+                              key: state.pageKey,
+                              child: const AlertDetailScreen(),
+                              transitionsBuilder: (context, animation,
+                                  secondaryAnimation, child) {
+                                const begin = Offset(-1.0, 0.0);
+                                const end = Offset.zero;
+                                const curve = Curves.easeInOut;
+
+                                var tween = Tween(begin: begin, end: end)
+                                    .chain(CurveTween(curve: curve));
+                                var offsetAnimation = animation.drive(tween);
+
+                                return SlideTransition(
+                                  position: offsetAnimation,
+                                  child: child,
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ]),
+            ]),
+        GoRoute(
+          path: Paths.verifyNumber,
+          name: Paths.verifyNumber,
+          builder: (context, state) {
+            return const VerifyNumberScreen();
+          },
+        ),
+        GoRoute(
+          path: Paths.forgotPassword,
+          name: Paths.forgotPassword,
+          builder: (context, state) {
+            return const ForgotPasswordScreen();
+          },
+        ),
+        GoRoute(
+          path: Paths.otpScreen,
+          name: Paths.otpScreen,
+          builder: (context, state) {
+            final List data = state.extra as List;
+            return OTPScreen(
+              vId: data[1],
+              phoneNumber: data[0],
+            );
+          },
+        ),
+      ],
     ),
   ],
 );
