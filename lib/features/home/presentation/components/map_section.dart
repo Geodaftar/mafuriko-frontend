95a1bdd3314a6b26ad22048ddffc5bf7e7a736b3
Cfcool100 <cfcool100@gmail.com>
2024-08-28 22:59:51 +0100

    [cf_map_ft] : replace google maps by "MapScreen"

--- lib/features/home/presentation/components/map_section.dart
+++ lib/features/home/presentation/components/map_section.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:mafuriko/core/routes/constant_path.dart';
 import 'package:mafuriko/features/home/presentation/widgets/home_widget.dart';
+import 'package:mafuriko/features/maps/presentation/bloc/map_bloc.dart';
+import 'package:mafuriko/features/maps/presentation/screens/map_screen.dart';
+import 'package:mafuriko/service_locator.dart';
 
 class MapSection extends StatelessWidget {
   const MapSection({
@@ -10,33 +14,29 @@ class MapSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.w),
-      child: Column(
-        children: [
-          const Section(
-            title: 'Carte',
-            routeTitle: 'Voir la carte',
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10.r),
-            child: SizedBox(
-              width: 335.w,
-              height: 112.h,
-              child: const GoogleMap(
-                initialCameraPosition: CameraPosition(
-                  target: LatLng(5.400686, -3.980562), // default position
-                  zoom: 10,
+    return BlocProvider(
+      create: (context) => sl<MapBloc>()..add(LoadUserLocationEvent()),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: Column(
+          children: [
+            const Section(
+              title: 'Carte',
+              routeTitle: 'Voir la carte',
+              // route: Paths.mapScreen,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.r),
+              child: SizedBox(
+                width: 335.w,
+                height: 112.h,
+                child: const MapScreen(
+                  enabledLocation: false,
                 ),
-                zoomControlsEnabled: false,
-                zoomGesturesEnabled: false,
-                scrollGesturesEnabled: false,
-                myLocationEnabled: true,
-                mapType: MapType.terrain,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
