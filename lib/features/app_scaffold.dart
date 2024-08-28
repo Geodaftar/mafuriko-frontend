fd0f8f743d4a07c301e747c8c801f45bd742ea7a
Cfcool100 <cfcool100@gmail.com>
2024-08-28 23:05:48 +0100

    [cf_map_ft] : app scaffold for shell route

--- lib/features/app_scaffold.dart
+++ lib/features/app_scaffold.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mafuriko/core/routes/constant_path.dart';
 import 'package:mafuriko/features/home/presentation/widgets/botton_nav_bar.dart';
 
 class ScaffoldApp extends StatefulWidget {
@@ -14,12 +16,29 @@ class ScaffoldApp extends StatefulWidget {
 }
 
 class _ScaffoldAppState extends State<ScaffoldApp> {
+  int _selectedIndex = 0;
+
+  final List<String> _routes = [Paths.home, Paths.mapScreen];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // Naviguer vers les routes définies
+    context.goNamed(_routes[index]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
       child: Scaffold(
-        bottomNavigationBar: const BottomNavBar(),
+        bottomNavigationBar: BottomNavBar(
+          index: _selectedIndex,
+          onTap: (i) {
+            _onItemTapped(i);
+          },
+        ),
         body: widget.child,
       ),
     );
