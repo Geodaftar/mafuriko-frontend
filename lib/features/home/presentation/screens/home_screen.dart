df6bad8ce46855e2f0eea84fac51d9517bb3d371
Cfcool100 <cfcool100@gmail.com>
2024-08-28 23:01:54 +0100

    [cf_map_ft] : initiate mapBloc into initState

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:mafuriko/features/home/presentation/components/home_sections.dart';
 import 'package:mafuriko/features/home/presentation/widgets/home_widget.dart';
+import 'package:mafuriko/features/maps/presentation/bloc/map_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,6 +15,24 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    // WidgetsBinding.instance.addPostFrameCallback(
+    //   (_) {
+    //     context.read<MapBloc>().add(LoadUserLocationEvent());
+    //   },
+    // );
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final mapBloc = context.read<MapBloc>();
+      if (mapBloc.isClosed) {
+        return;
+      }
+      mapBloc.add(LoadUserLocationEvent());
+    });
+  }
 
   @override
   void dispose() {
