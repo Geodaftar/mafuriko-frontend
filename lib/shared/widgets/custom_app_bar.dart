d1a4c9aff270669b08b1e7870bb0ae0f6456b088
Cfcool100 <cfcool100@gmail.com>
2024-09-09 15:54:45 +0100

    [cf_profile_ft] : refactor comment back app bar

---
+++ lib/shared/widgets/custom_app_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mafuriko/gen/gen.dart';
+
+class AppBackAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const AppBackAppBar({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Size get preferredSize => Size.fromHeight(70.h);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      titleSpacing: 2,
+      leadingWidth: 70.w,
+      leading: InkWell(
+        overlayColor: const WidgetStatePropertyAll(Colors.transparent),
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Container(
+          width: 10.w,
+          height: 20.h,
+          padding: EdgeInsets.symmetric(horizontal: 13.w),
+          child: Padding(
+            padding: EdgeInsets.only(left: 1.w),
+            child: Icon(Icons.arrow_back_ios, size: 20.h),
+          ),
+        ),
+      ),
+      title: InkWell(
+        overlayColor: const WidgetStatePropertyAll(Colors.transparent),
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Text(
+          title,
+          style: TextStyle(
+            color: AppColor.primary,
+            fontSize: 16.sp,
+            fontFamily: AppFonts.nunito,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+}
