3ec8903f3473464d99bab7861c1225c9c9619070
Cfcool100 <cfcool100@gmail.com>
2024-09-29 20:25:48 +0100

    [cf_send-alert_ft] : rollback to GoogleMap

--- lib/features/maps/presentation/screens/map_screen.dart
+++ lib/features/maps/presentation/screens/map_screen.dart
@@ -1,16 +1,16 @@
 import 'dart:async';
 
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import 'package:latlong2/latlong.dart';
-import 'package:mafuriko/core/constant_secret.dart';
 import 'package:mafuriko/core/routes/constant_path.dart';
 import 'package:mafuriko/features/maps/presentation/bloc/map_bloc.dart';
+import 'package:mafuriko/features/send/domain/entities/alert_entity.dart';
+import 'package:mafuriko/features/send/presentation/bloc/alert_bloc.dart';
 import 'package:mafuriko/gen/gen.dart';
 import 'package:mafuriko/service_locator.dart';
 
@@ -23,24 +23,107 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  final _mapController = MapController();
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+  final _customInfoWindowController = CustomInfoWindowController();
+
+  List<Marker> _markers = [];
+
+  bool isLoad = false;
+
+  LatLng currentPosition = const LatLng(0.393037, 9.450152);
 
   @override
   void initState() {
     super.initState();
-    // _determinePosition();
-    _alignPositionOnUpdate = AlignOnUpdate.always;
-    _alignPositionStreamController = StreamController<double?>();
-    // context.read<MapBloc>().add(LoadUserLocationEvent());
   }
 
-  AlignOnUpdate? _alignPositionOnUpdate;
-  StreamController<double?>? _alignPositionStreamController;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Safe to use context.watch here
+    _getFloodData(context);
+  }
+
+  void _getFloodData(BuildContext context) {
+    final List<AlertEntity> alerts = context.watch<AlertBloc>().state.alerts;
+
+    setState(() {
+      _markers = alerts.map(
+        (alert) {
+          return Marker(
+            icon: AssetMapBitmap(AppImages.icons.alertPinPng.path),
+            markerId: MarkerId(alert.id),
+            position: LatLng(
+              double.parse(alert.floodLocation?['latitude']),
+              double.parse(alert.floodLocation?['longitude']),
+            ),
+            onTap: () {
+              // call custom window to display alert
+              _customInfoWindowController.addInfoWindow!(
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 8.w, vertical: 12.h),
+                  decoration: ShapeDecoration(
+                    color: Color(0xFFF8EBE1),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1.w, color: AppColor.primary),
+                      borderRadius: BorderRadius.circular(10.r),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        alert.floodScene ?? 'N/A',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: AppColor.primary,
+                          fontSize: 12.sp,
+                          fontFamily: AppFonts.nunito,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        'Inondation',
+                        style: TextStyle(
+                          color: AppColor.primary,
+                          fontSize: 12.sp,
+                          fontFamily: AppFonts.nunito,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 56.h,
+                        child: Text(
+                          alert.floodDescription ?? 'N/A',
+                          style: TextStyle(
+                            color: AppColor.primaryGray,
+                            fontSize: 10.sp,
+                            fontFamily: AppFonts.nunito,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                LatLng(
+                  double.parse(alert.floodLocation?['latitude']),
+                  double.parse(alert.floodLocation?['longitude']),
+                ),
+              );
+            },
+          );
+        },
+      ).toList();
+    });
+  }
 
   @override
   void dispose() {
-    _mapController.dispose();
-    _alignPositionStreamController?.close();
+    _customInfoWindowController.dispose();
     super.dispose();
   }
 
@@ -57,115 +140,65 @@ class _MapScreenState extends State<MapScreen> {
         },
         builder: (context, state) {
           if (state is MapLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is MapLoaded) {
-            return FlutterMap(
-              mapController: _mapController,
-              options: MapOptions(
-                initialZoom: 14,
-                // backgroundColor: Colors.black,
-                cameraConstraint: CameraConstraint.contain(
-                  bounds: LatLngBounds(
-                    const LatLng(-90, -180),
-                    const LatLng(90, 180),
-                  ),
-                ),
-                onPositionChanged: (MapCamera camera, bool hasGesture) {
-                  if (hasGesture &&
-                      _alignPositionOnUpdate != AlignOnUpdate.never) {
-                    setState(
-                      () => _alignPositionOnUpdate = AlignOnUpdate.never,
-                    );
-                  }
-                },
+            return GoogleMap(
+              key: ValueKey(isLoad),
+              mapType: MapType.normal,
+              initialCameraPosition: CameraPosition(
+                target: const LatLng(4.027265, -9.027265),
+                zoom: widget.enabledLocation ? 15.0 : 16.0,
               ),
+              onMapCreated: (GoogleMapController controller) {
+                _customInfoWindowController.googleMapController = controller;
+                _controller.complete(controller);
+              },
+              onTap: (argument) {
+                // _customInfoWindowController.hideInfoWindow!();
+              },
+              onCameraMove: (position) {
+                _customInfoWindowController.onCameraMove!();
+              },
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              zoomControlsEnabled: false,
+              markers: widget.enabledLocation ? _markers.toSet() : {},
+              zoomGesturesEnabled: widget.enabledLocation ? true : false,
+            );
+          } else if (state is MapLoaded) {
+            return Stack(
               children: [
-                TileLayer(
-                  additionalOptions: {
-                    'accessToken': Secrets.accessToken,
-                    'id': 'mapbox.mapbox-streets-v7',
+                GoogleMap(
+                  key: ValueKey(isLoad),
+                  mapType: MapType.normal,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(state.position?.latitude ?? 0,
+                        state.position?.longitude ?? 0),
+                    zoom: widget.enabledLocation ? 15.0 : 16.0,
+                  ),
+                  // style: theme,
+                  onMapCreated: (GoogleMapController controller) {
+                    _customInfoWindowController.googleMapController =
+                        controller;
+                    _controller.complete(controller);
                   },
-                  urlTemplate:
-                      'https://api.mapbox.com/styles/v1/${Secrets.userId}/${Secrets.styleId}/tiles/256/{z}/{x}/{y}@2x?access_token=${Secrets.accessToken}',
-                  maxZoom: 19,
-                  userAgentPackageName: 'com.geodaftar.mafuriko',
+                  onTap: (argument) {
+                    // _customInfoWindowController.hideInfoWindow!();
+                  },
+                  onCameraMove: (position) {
+                    _customInfoWindowController.onCameraMove!();
+                  },
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: false,
+                  zoomControlsEnabled: false,
+                  markers: widget.enabledLocation ? _markers.toSet() : {},
+                  zoomGesturesEnabled: widget.enabledLocation ? true : false,
+                  scrollGesturesEnabled: widget.enabledLocation ? true : false,
                 ),
-                MarkerLayer(
-                  markers: widget.enabledLocation == false
-                      ? []
-                      : [
-                          Marker(
-                            width: 70.w,
-                            height: 70.h,
-                            point: const LatLng(0.3956, 9.4543),
-                            child: CircleAvatar(
-                              backgroundColor: const Color(0x857A4411),
-                              child: Icon(
-                                size: 38.dm,
-                                Icons.flood_outlined,
-                                color: AppColor.black,
-                              ),
-                            ),
-                          ),
-                          Marker(
-                            width: 70.w,
-                            height: 70.h,
-                            point: const LatLng(0.3823, 9.4673),
-                            child: CircleAvatar(
-                              backgroundColor: const Color(0x857A4411),
-                              child: Icon(
-                                size: 38.dm,
-                                Icons.flood_outlined,
-                                color: AppColor.black,
-                              ),
-                            ),
-                          ),
-                          Marker(
-                            width: 70.w,
-                            height: 70.h,
-                            point: const LatLng(0.4003, 9.5306),
-                            child: CircleAvatar(
-                              backgroundColor: const Color(0x857A4411),
-                              child: Icon(
-                                size: 38.dm,
-                                Icons.flood_outlined,
-                                color: AppColor.black,
-                              ),
-                            ),
-                          ),
-                        ],
+                CustomInfoWindow(
+                  controller: _customInfoWindowController,
+                  width: 167.w,
+                  height: 137.10.h,
+                  offset: 60,
                 ),
-                CurrentLocationLayer(
-                  alignPositionStream: _alignPositionStreamController?.stream,
-                  alignPositionOnUpdate: _alignPositionOnUpdate,
-                ),
-                widget.enabledLocation
-                    ? Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: FloatingActionButton(
-                            backgroundColor: AppColor.primary,
-                            shape: const CircleBorder(),
-                            onPressed: () {
-                              // Align the location marker to the center of the map widget
-                              // on location update until user interact with the map.
-                              setState(
-                                () => _alignPositionOnUpdate =
-                                    AlignOnUpdate.always,
-                              );
-                              // Align the location marker to the center of the map widget
-                              // and zoom the map to level 18.
-                              _alignPositionStreamController?.add(14);
-                            },
-                            child: const Icon(
-                              Icons.my_location,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      )
-                    : Container(),
               ],
             );
           } else if (state is MapError) {
