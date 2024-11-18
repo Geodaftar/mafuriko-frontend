a578c1a5a79f2dc6b459e53ffd86eba9fc0769e9
Cfcool100 <cfcool100@gmail.com>
2024-11-18 22:29:29 +0100

    [cf_profile_ft_2] : update params field

--- lib/features/authentication/domain/repositories/auth_repository.dart
+++ lib/features/authentication/domain/repositories/auth_repository.dart
@@ -7,7 +7,6 @@ import 'package:mafuriko/shared/errors/failures.dart';
 abstract interface class AuthRepository {
   Future<Either<Failure, UserEntity>> signUp({
     required String userEmail,
-    required String userName,
     required String userNumber,
     required String userPassword,
     required String confirmPassword,
