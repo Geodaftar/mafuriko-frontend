7f113847470a83b7b61d376ec87fe6d85bbb95f6
Cfcool100 <cfcool100@gmail.com>
2024-12-13 10:52:20 +0100

    [cf_profile_ft_2] : add navigation to alerts list

--- lib/features/home/presentation/widgets/report_count.dart
+++ lib/features/home/presentation/widgets/report_count.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mafuriko/core/routes/constant_path.dart';
 import 'package:mafuriko/features/send/domain/entities/alert_entity.dart';
 import 'package:mafuriko/features/send/presentation/bloc/alert_bloc.dart';
 import 'package:mafuriko/gen/gen.dart';
@@ -12,50 +14,53 @@ class ReportCount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.fromLTRB(0, 16.h, 18.w, 16.h),
-      padding: EdgeInsets.all(10.dm),
-      decoration: ShapeDecoration(
-        color: AppColor.primary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(7.r),
+    return InkWell(
+      onTap: () => context.goNamed(Paths.alertsScreen),
+      child: Container(
+        margin: EdgeInsets.fromLTRB(0, 16.h, 18.w, 16.h),
+        padding: EdgeInsets.all(10.dm),
+        decoration: ShapeDecoration(
+          color: AppColor.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(7.r),
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Nombre de rapports reçus ',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 14.sp,
-              fontFamily: AppFonts.nunito,
-              fontWeight: FontWeight.w600,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Nombre de rapports reçus ',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14.sp,
+                fontFamily: AppFonts.nunito,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          SizedBox(width: 10.w),
-          BlocBuilder<AlertBloc, AlertState>(
-            builder: (context, state) {
-              List<AlertEntity> alerts = [];
+            SizedBox(width: 10.w),
+            BlocBuilder<AlertBloc, AlertState>(
+              builder: (context, state) {
+                List<AlertEntity> alerts = [];
 
-              for (var alert in state.alerts) {
-                if (alert.status == 'success') {
-                  alerts.add(alert);
+                for (var alert in state.alerts) {
+                  if (alert.status == 'success') {
+                    alerts.add(alert);
+                  }
                 }
-              }
 
-              return Text(
-                '${alerts.length}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.sp,
-                  fontFamily: AppFonts.nunito,
-                  fontWeight: FontWeight.w700,
-                ),
-              );
-            },
-          ),
-        ],
+                return Text(
+                  '${alerts.length}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.sp,
+                    fontFamily: AppFonts.nunito,
+                    fontWeight: FontWeight.w700,
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
